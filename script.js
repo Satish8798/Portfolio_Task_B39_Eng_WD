58c805b2094c7241d694f9508378439d67507b36
Satish Ganesan <ganesansatishm7@gmail.com>
2022-09-26 19:02:30 +0530

    added interests, acievements in About and manymore changes

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.right="-200px";
 }
+
+/* experience */
+
+let experienceTabLinks=document.getElementsByClassName('exp-tab-links');
+let experienceTabContents= document.getElementsByClassName('exp-tab-contents');
+function openTabExp(tabName){
+    for(let i=0;i<experienceTabLinks.length;i++){
+        tabLinks[i].classList.remove('exp-active-link');
+    }
+
+    for(let tabContent of experienceTabContents){
+        tabContent.classList.remove('exp-active-tab')
+    }
+
+    event.currentTarget.classList.add('exp-active-link');
+    document.getElementById(tabName).classList.add('exp-active-tab')
+}
